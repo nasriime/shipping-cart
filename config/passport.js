0e4d4251c4a3381f37359f6c42a095ef7c9daaa5
nasriime <mnasr4040@gmail.com>
2019-03-30 02:20:03 +0200

    signin local strategy

--- config/passport.js
+++ config/passport.js
@@ -46,3 +46,34 @@ passport.use('local.signup', new localStrategy({
         })
     })
 }));
+
+passport.use('local.signin',new localStrategy({
+    usernameField: 'email',
+    passwordField: 'password',
+    passReqToCallback: true
+},function(req,email,password,done){
+    req.checkBody('email','Invaled email').notEmpty().isEmail();
+    req.checkBody('password','Invalied password').notEmpty();
+    const errors = req.validationErrors();
+    if(errors){
+        let messages = [];
+        errors.forEach(error=>{
+            messages.push(error.msg)
+        })
+        return done(null,false,req.flash('error',messages))
+    }
+
+    User.findOne({email},function(err,user){
+        if(err){
+            return done(err)
+        }
+        if(!user){
+            return done(null, false, {message: 'User not found!'});
+        }
+
+        if(!user.validatePassword(password)){
+            return done(null,false,{message: 'Wrong password!'})
+        }
+        return done(null,user);
+    })
+}));
