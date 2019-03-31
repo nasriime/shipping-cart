2acde6a40164b8bb30a6034b3913eb17b0d283e4
nasriime <mnasr4040@gmail.com>
2019-03-31 23:15:54 +0200

    add cart model

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const Product = require('../models/product');
+const Cart = require('../models/cart');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -18,6 +19,14 @@ router.get('/', function(req, res, next) {
 
 router.get('/add-to-cart/:id',function(req, res, next){
   var productId = req.params.id;
+  const cart = new Cart(req.session.cart ? req.session.cart : {} );
+  Product.findById(productId,function(err,product){
+    if (err) {return res.redirect('/');}
+    cart.add(product, product.id);
+    req.session.cart = cart;
+    console.log(req.session.cart)
+    res.redirect('/');
+  })
 })
 
 module.exports = router;
